08b186a2d1700b492f94e56aaca8a44bba7266c6
Miss <emesquitta73@gmail.com>
2021-04-11 13:06:47 +0100

    added countries component

---
+++ src/Components/Countries.js
@@ -0,0 +1,12 @@
+import React,{ useState, useEffect} from 'react'
+import countriesAll from "./countriesAll.js"
+
+const Countries = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Countries
